44e4461b3bcbbc5535483a39da5885c3267ba120
Sidharth Sharma <sidsharma27@users.noreply.github.com>
2016-09-28 13:19:36 -0700

    2FANoPush

--- NexmoBankingApp/AppDelegate.swift
+++ NexmoBankingApp/AppDelegate.swift
@@ -16,14 +16,10 @@ import Parse
 import VerifyIosSdk
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GGLInstanceIDDelegate, GCMReceiverDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
-    var registrationOptions = [String: AnyObject]()
-    var registrationToken: String?
-    
-    let gcmSenderId = "GCMSENDERID"
+        
     private var appID = "APP_ID"
     private var sharedSecret = "SHARED_SECRET"
     
@@ -39,56 +35,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GGLInstanceIDDelegate, GC
         
         NexmoClient.start(applicationId: appID, sharedSecretKey: sharedSecret)
         
-        let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
-        application.registerUserNotificationSettings(settings)
-        application.registerForRemoteNotifications()
         return true
     }
-    
-    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-        // Initialise GCM
-        let instanceIDConfig = GGLInstanceIDConfig.defaultConfig()
-        instanceIDConfig.delegate = self
-        // Start the GGLInstanceID shared instance with that config and request a registration token to enable reception of notifications
-        GGLInstanceID.sharedInstance().startWithConfig(instanceIDConfig)
-        registrationOptions = [kGGLInstanceIDRegisterAPNSOption:deviceToken,
-                               kGGLInstanceIDAPNSServerTypeSandboxOption:true]
-        GGLInstanceID.sharedInstance().tokenWithAuthorizedEntity(gcmSenderId,
-                                                                 scope: kGGLInstanceIDScopeGCM, options: registrationOptions, handler: self.handleGcmToken)
-        print("registered for push notifications")
-    }
-    
-    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        if (VerifyClient.handleNotification(userInfo, performSilentCheck: false )) {
-            print("Notification recieved.")
-            return
-        }
-    }
-    
-    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-        if error.code == 3010 {
-            print("Push notifications are not supported in the iOS Simulator.")
-        } else {
-            print("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
-        }
-    }
-    
-    
-    func onTokenRefresh() {
-        print("The GCM registration token needs to be changed.")
-        GGLInstanceID.sharedInstance().tokenWithAuthorizedEntity(gcmSenderId, scope: kGGLInstanceIDScopeGCM, options: registrationOptions, handler: self.handleGcmToken)
-    }
-    
-    func handleGcmToken(token: String!, error: NSError!) {
-        if let error = error {
-            print("failed to get gcm token with error \(error.localizedDescription)")
-        } else {
-            print("gcm token:\n\(token)")
-            // provide nexmo client with gcm token
-            NexmoClient.setGcmToken(token)
-        }
-    }
+ 
     
     func applicationWillResignActive(application: UIApplication) { }
     
--- NexmoBankingApp/NexmoBankingApp-Bridging-Header.h
+++ NexmoBankingApp/NexmoBankingApp-Bridging-Header.h
@@ -1,6 +1,5 @@
 #ifndef NexmoBankingApp_Bridging_Header_h
 #define NexmoBankingApp_Bridging_Header_h
 
-#import <Google/CloudMessaging.h>
 
 #endif /* NexmoBankingApp_Bridging_Header_h */
